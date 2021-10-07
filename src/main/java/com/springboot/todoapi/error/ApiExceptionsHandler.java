b0787b7b5677e07320335f8e19298cc6ec3ee54b
Med arb <arb.med241@gmail.com>
2021-10-07 11:59:07 +0200

    handling list of errors

--- src/main/java/com/springboot/todoapi/error/ApiExceptionsHandler.java
+++ src/main/java/com/springboot/todoapi/error/ApiExceptionsHandler.java
@@ -1,10 +1,17 @@
 package com.springboot.todoapi.error;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 @ControllerAdvice
 public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
@@ -12,5 +19,18 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorDetails> handlerApiExceptions(ApiBaseException ex, WebRequest request){
         ErrorDetails details = new ErrorDetails(ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(details, ex.getStatusCode());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ValidationError validationError = new ValidationError();
+        validationError.setUri(request.getDescription(false));
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        for(FieldError fieldError: fieldErrors) {
+            validationError.addError(fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(validationError, HttpStatus.BAD_REQUEST);
     }
 }
