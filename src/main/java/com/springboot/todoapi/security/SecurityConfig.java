5ea781a68d565d7bfac063e6da1fb5a60567ce14
marbaoui <arb.med241@gmail.com>
2021-10-20 18:17:16 +0200

    add jwt auth

--- src/main/java/com/springboot/todoapi/security/SecurityConfig.java
+++ src/main/java/com/springboot/todoapi/security/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.springboot.todoapi.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final String[] PUBLIC_ENDPOINTS ={
             "/api/v1/auth/**",
     };
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
